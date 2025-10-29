166542b050c2c06062cbbb3892ffc77fa7a5c22a
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-29 19:21:44 +0530

    Update provider.tf

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "remote-state-pavan"
-    key = "vpc-module-demo"
+    key = "roboshop-dev-vpc"
     region = "us-east-1"
     use_lockfile = true 
     encrypt = true 
