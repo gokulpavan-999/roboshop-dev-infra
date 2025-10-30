2dcd63287f22674332409344e8a5765ab77787ea
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-30 16:03:13 +0530

    Update main.tf

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -1,1 +1,15 @@
-
+resource "aws_instance" "bastion" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.bastion_sg_id]
+    #subnet_id = local.public_subnet_id
+    #iam_instance_profile = aws_iam_instance_profile.bastion.name
+    #user_data = file("bastion.sh")
+    
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${var.project_name}-${var.environment}-bastion"
+        }
+    )
+}
