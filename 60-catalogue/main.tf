c8cd7e967aee5c795e436c0ef984c019ae81f5a7
gokulpavan-999 <pavanthota480@gmail.com>
2025-11-10 19:27:46 +0530

    Update main.tf

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -167,3 +167,34 @@ resource "aws_autoscaling_group" "catalogue" {
   }
 
 }
+
+resource "aws_autoscaling_policy" "catalogue" {
+  autoscaling_group_name = aws_autoscaling_group.catalogue.name
+  name                   = "${local.common_name_suffix}-catalogue"
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 75.0
+  }
+}
+
+resource "aws_lb_listener_rule" "catalogue" {
+  listener_arn = local.backend_alb_listener_arn
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.catalogue.arn
+  }
+
+  condition {
+    host_header {
+      values = ["catalogue.backend-alb-${var.environment}.${var.domain_name}"]
+    }
+  }
+}
+
