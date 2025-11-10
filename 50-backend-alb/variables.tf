bffa48e30c7616b30e3b75a03643dac034990876
gokulpavan-999 <pavanthota480@gmail.com>
2025-11-10 19:12:37 +0530

    Update variables.tf

--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z06390712DSNMTA6S2AHD"
+}
+
+variable "domain_name" {
+    default = "pavandevops.fun"
+}
