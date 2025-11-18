9ed3805199ae7b77077f9c31cb9f147d189485cc
gokulpavan-999 <pavanthota480@gmail.com>
2025-11-18 12:26:46 +0530

    Update main.tf

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "bastion" {
     vpc_security_group_ids = [local.bastion_sg_id]
     subnet_id = local.public_subnet_id
     iam_instance_profile = aws_iam_instance_profile.bastion.name
-    user_data = file("bastion.sh")
+    #user_data = file("bastion.sh")
 
     # need more for terraform
     root_block_device {
