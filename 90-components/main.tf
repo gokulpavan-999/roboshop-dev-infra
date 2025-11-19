48432752081a84e13204c1e4f2372751a6fd5e8c
gokulpavan-999 <pavanthota480@gmail.com>
2025-11-19 18:07:47 +0530

    Update main.tf

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,1 +1,13 @@
 
+/* module "components" {
+    source = "../../terraform-roboshop-component"
+    component = var.component
+    rule_priority = var.rule_priority
+}
+ */
+module "components" {
+    for_each = var.components
+    source = "git::https://github.com/gokulpavan-999/terraform-roboshop-component.git?ref=main"
+    component = each.key
+    rule_priority = each.value.rule_priority
+}
